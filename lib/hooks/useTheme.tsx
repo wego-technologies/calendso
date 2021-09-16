c8485edae6554dadf32c367575753b456c91161c
Aguilaair <eduardomoreno.eduardo@gmail.com>
2021-09-16 22:02:07 +0200

    Disable dark theme

--- lib/hooks/useTheme.tsx
+++ lib/hooks/useTheme.tsx
@@ -5,7 +5,7 @@ export default function useTheme(theme?: string) {
   const [isReady, setIsReady] = useState(false);
   useEffect(() => {
     if (!theme && window.matchMedia("(prefers-color-scheme: dark)").matches) {
-      document.documentElement.classList.add("dark");
+      //document.documentElement.classList.add("dark");
     } else {
       document.documentElement.classList.add(theme);
     }
