fd6a436c1e1b68add716bc06c041320bfbac2485
Aguilaair <eduardomoreno.eduardo@gmail.com>
2021-09-16 22:28:17 +0200

    Restyle datepicker

--- components/booking/DatePicker.tsx
+++ components/booking/DatePicker.tsx
@@ -132,7 +132,7 @@ const DatePicker = ({
           ? "w-full sm:w-1/2 md:w-1/3 sm:border-r sm:dark:border-gray-800 sm:pl-4 sm:pr-6 "
           : "w-full sm:pl-4")
       }>
-      <div className="flex text-gray-600 font-light text-xl mb-4">
+      <div className="flex mb-4 text-xl font-light text-gray-600">
         <span className="w-1/2 text-gray-600 dark:text-white">
           <strong className="text-gray-900 dark:text-white">{inviteeDate().format("MMMM")}</strong>
           <span className="text-gray-500"> {inviteeDate().format("YYYY")}</span>
@@ -144,18 +144,18 @@ const DatePicker = ({
               "group mr-2 p-1" + (selectedMonth <= dayjs().month() && "text-gray-400 dark:text-gray-600")
             }
             disabled={selectedMonth <= dayjs().month()}>
-            <ChevronLeftIcon className="group-hover:text-black dark:group-hover:text-white w-5 h-5" />
+            <ChevronLeftIcon className="w-5 h-5 group-hover:text-black dark:group-hover:text-white" />
           </button>
-          <button className="group p-1" onClick={incrementMonth}>
-            <ChevronRightIcon className="group-hover:text-black dark:group-hover:text-white w-5 h-5" />
+          <button className="p-1 group" onClick={incrementMonth}>
+            <ChevronRightIcon className="w-5 h-5 group-hover:text-black dark:group-hover:text-white" />
           </button>
         </div>
       </div>
-      <div className="grid grid-cols-7 gap-4 text-center border-b border-t dark:border-gray-800 sm:border-0">
+      <div className="grid grid-cols-7 gap-4 text-center border-t border-b dark:border-gray-800 sm:border-0">
         {["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
           .sort((a, b) => (weekStart.startsWith(a) ? -1 : weekStart.startsWith(b) ? 1 : 0))
           .map((weekDay) => (
-            <div key={weekDay} className="uppercase text-gray-500 text-xs tracking-widest my-4">
+            <div key={weekDay} className="my-4 text-xs tracking-widest text-gray-500 uppercase">
               {weekDay}
             </div>
           ))}
@@ -167,7 +167,7 @@ const DatePicker = ({
             style={{
               paddingTop: "100%",
             }}
-            className="w-full relative">
+            className="relative w-full">
             {day === null ? (
               <div key={`e-${idx}`} />
             ) : (
@@ -175,13 +175,13 @@ const DatePicker = ({
                 onClick={() => onDatePicked(inviteeDate().date(day.date))}
                 disabled={day.disabled}
                 className={classNames(
-                  "absolute w-full top-0 left-0 right-0 bottom-0 rounded-sm text-center mx-auto",
-                  "hover:border hover:border-black dark:hover:border-white",
+                  "absolute w-full top-0 left-0 right-0 bottom-0 rounded-sm text-center mx-auto rounded-lg",
+                  "hover:border-4 hover:border-blue-600 dark:hover:border-white",
                   day.disabled
                     ? "text-gray-400 font-light hover:border-0 cursor-default"
                     : "dark:text-white text-primary-500 font-medium",
                   date && date.isSame(inviteeDate().date(day.date), "day")
-                    ? "bg-black text-white-important"
+                    ? "bg-blue-600 text-white-important"
                     : !day.disabled
                     ? " bg-gray-100 dark:bg-gray-600"
                     : ""
