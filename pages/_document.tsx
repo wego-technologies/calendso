5e8db166ee52e64eceaab114159588f0b758d983
Aguilaair <eduardomoreno.eduardo@gmail.com>
2021-09-21 21:04:14 +0200

    Add tag manager

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -19,8 +19,25 @@ class MyDocument extends Document<Props> {
           <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#000000" />
           <meta name="msapplication-TileColor" content="#ff0000" />
           <meta name="theme-color" content="#ffffff" />
+          <script
+            async
+            src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GTM});`,
+            }}></script>
         </Head>
-        <body className="dark:bg-black bg-gray-100">
+        <body className="bg-gray-100 dark:bg-black">
+          <noscript
+            dangerouslySetInnerHTML={{
+              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${process.env.NEXT_PUBLIC_GTM}"
+            height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+            }}></noscript>
           <Main />
           <NextScript />
         </body>
