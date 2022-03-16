794c591b843ec0e4754425a1aeb8d32f5ee05ed5
Eduardo M <contact@eduardom.dev>
2022-03-16 17:36:04 +0100

    Fix GTM and tracking

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -25,12 +25,22 @@ class MyDocument extends Document<Props> {
           />
           <script
             dangerouslySetInnerHTML={{
-              __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer',${process.env.NEXT_PUBLIC_GTM});`,
-            }}></script>
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GTM}', {
+              page_path: window.location.pathname,
+            });
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+            }}
+          />
         </Head>
         <body className="bg-gray-100 dark:bg-black">
           <noscript
