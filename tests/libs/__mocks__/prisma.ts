ae8527433018cce2193d862f3cb12aa6f88b2854
Eduardo M <contact@eduardom.dev>
2023-08-25 11:50:31 +0200

    Add 3.2

---
+++ tests/libs/__mocks__/prisma.ts
@@ -0,0 +1,17 @@
+import { beforeEach, vi } from "vitest";
+import { mockDeep, mockReset } from "vitest-mock-extended";
+
+import type { PrismaClient } from "@calcom/prisma";
+
+vi.mock("@calcom/prisma", () => ({
+  default: prisma,
+  availabilityUserSelect: vi.fn(),
+  userSelect: vi.fn(),
+}));
+
+beforeEach(() => {
+  mockReset(prisma);
+});
+
+const prisma = mockDeep<PrismaClient>();
+export default prisma;
